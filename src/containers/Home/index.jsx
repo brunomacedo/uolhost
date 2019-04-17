f9ff9207a87c3aadf8ceb37ac1f4600943932c92
Bruno Macedo <contato@brunomacedo.com.br>
2019-04-17 17:51:43 -0300

    [FEAT] Home view

---
+++ src/containers/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => (
+  <React.Fragment>
+    <h1 className="bg-red">red</h1>
+    <p className="bg-green">theme</p>
+    <p className="bg-gray">gray</p>
+  </React.Fragment>
+);
+
+export default Home;
