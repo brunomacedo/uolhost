434a544133c640ba7400fe5dae8a43c9112845d7
Bruno Macedo <contato@brunomacedo.com.br>
2019-05-04 12:51:09 -0300

    [FEAT] call components in HomePage

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,14 +1,51 @@
-import React, { Fragment } from 'react';
-import Header from '../../components/Header';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import userIcon from '../../assets/images/user.png';
 import Container from '../../components/Container';
+import { loadCustomers } from '../../store/actions/customers';
+import ListCard from '../../components/ListCard';
 
-const Home = () => (
-  <Fragment>
-    <Header />
-    <Container>
-      <button className="btn bg-theme" type="button">Novo cliente</button>
-    </Container>
-  </Fragment>
-);
+class Home extends Component {
+  componentDidMount() {
+    const { loadCustomers: propsLoadCustomers } = this.props;
+    propsLoadCustomers();
+  }
 
-export default Home;
+  render() {
+    const { customers } = this.props;
+    return (
+      <Container
+        header={{
+          icon: userIcon,
+          component: 'h1',
+          text: 'Painel de clientes',
+        }}
+        subHeader={{
+          title: 'Listagem de usuários',
+          description: 'Escolha um cliente para visualizar os detalhes',
+          button: {
+            text: 'Novo cliente',
+            classes: 'btn--theme',
+          },
+        }}
+      >
+        <ListCard list={customers} />
+      </Container>
+    );
+  }
+}
+
+Home.propTypes = {
+  customers: PropTypes.instanceOf(Object).isRequired,
+  loadCustomers: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+  customers: state.customersReducer.customers,
+});
+
+export default connect(mapStateToProps, {
+  loadCustomers,
+})(Home);
