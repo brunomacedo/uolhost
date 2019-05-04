7377b46c4f9db46e51eca56287dae119153bb457
Bruno Macedo <contato@brunomacedo.com.br>
2019-05-04 12:49:10 -0300

    [FEAT] create main container

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import './container.sass';
+
+import Header from '../Header';
 import HeaderPage from '../HeaderPage';
-import userIcon from '../../assets/images/user.png';
+import SubHeaderPage from '../SubHeaderPage';
 
 const Container = (props) => {
-  const { children } = props;
+  const { children, header, subHeader } = props;
 
   return (
-    <section className="container">
-      <HeaderPage
-        icon={userIcon}
-        component="h3"
-        text="Painel de clientes"
-      />
-      {children}
-    </section>
+    <Fragment>
+      <Header />
+      <section className="container">
+        <HeaderPage {...header} />
+        <SubHeaderPage {...subHeader} />
+        {children}
+      </section>
+    </Fragment>
   );
 };
 
 Container.propTypes = {
   children: PropTypes.instanceOf(Object).isRequired,
+  header: PropTypes.instanceOf(Object).isRequired,
+  subHeader: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default Container;
