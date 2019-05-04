3f5bb2bc566af65616ec1e8efb5a20e0129f4d88
Bruno Macedo <contato@brunomacedo.com.br>
2019-05-04 18:27:40 -0300

    [FEAT] create component listCards

--- src/components/ListCard/index.jsx
+++ src/components/ListCard/index.jsx
@@ -1,28 +1,72 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import './list-card.sass';
+import withStyles from 'react-jss';
 
-const ListCard = ({ list }) => (
-  <ul>
-    {list.length ? (
-      list.map((item) => {
-        const { _id: itemId, name, contact } = item;
-        return (
-          <li key={itemId}>
-            {name}
-            <br />
-            {contact.email}
-          </li>
-        );
-      })
-    ) : (
-      <li>Nenhum item disponível...</li>
-    )
-  }
-  </ul>
+/* Just a simple example how to use JSS */
+const styles = {
+  status: {
+    display: 'inline-block',
+    verticalAlign: 'middle',
+    borderRadius: '50%',
+    width: '14px',
+    height: '14px',
+    marginRight: '8px',
+    marginTop: '-2px',
+  },
+  disclaimer: {
+    padding: '30px 0',
+  },
+};
+
+const ListCard = ({ list, classes }) => (
+  <Fragment>
+    <ul className="listStyle">
+      {list.length ? (
+        list.map((item) => {
+          const {
+            _id: itemId,
+            name,
+            cpf,
+            status,
+            contact,
+          } = item;
+          return (
+            <li key={itemId}>
+              <div>
+                <strong>{name}</strong>
+                <br />
+                {contact.email}
+              </div>
+              <div>
+                <strong>{cpf}</strong>
+                <br />
+                {contact.tel}
+              </div>
+              <div>
+                <span className={`status-${status.split(' ')[0].toLowerCase()} ${classes.status}`} />
+                {status}
+              </div>
+              <div>
+                <button className="btn btn--theme" type="button">Editar</button>
+              </div>
+            </li>
+          );
+        })
+      ) : (
+        <li>Nenhum item disponível...</li>
+      )
+    }
+    </ul>
+    <p className={classes.disclaimer}>
+      {`Exibindo ${list.length} clientes`}
+    </p>
+  </Fragment>
 );
 
 ListCard.propTypes = {
+  classes: PropTypes.instanceOf(Object).isRequired,
   list: PropTypes.instanceOf(Object).isRequired,
 };
 
-export default ListCard;
+export default withStyles(styles)(ListCard);
