d8818b80ff322435451d6e8901c01dc38afd8c1a
Bruno Macedo <contato@brunomacedo.com.br>
2019-05-04 12:50:47 -0300

    [FEAT] pass props to SubHeader in Container

---
+++ src/components/SubHeaderPage/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './sub-header-page.sass';
+
+const SubHeaderPage = (props) => {
+  const {
+    title,
+    description,
+    button,
+  } = props;
+
+  return (
+    <article className="page-article">
+      <header>
+        {title && <h2>{title}</h2>}
+        {description && <h3>{description}</h3>}
+      </header>
+      {button && (
+        <button
+          className={`btn ${button.classes}`}
+          type="button"
+        >
+          {button.text}
+        </button>
+      )}
+    </article>
+  );
+};
+
+SubHeaderPage.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  button: PropTypes.instanceOf(Object).isRequired,
+};
+
+SubHeaderPage.defaultProps = {
+  title: String(),
+  description: String(),
+};
+
+export default SubHeaderPage;
