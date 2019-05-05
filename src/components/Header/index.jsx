b13656956b23d477fe2b6ea2de47eed0c318470c
Bruno Macedo <contato@brunomacedo.com.br>
2019-05-05 01:01:34 -0300

    [FEAT] home link when click on logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import Logo from '../../assets/images/logo_uol_branco.svg';
 import './header.sass';
 
 const Header = () => (
   <header className="main_header bg-black">
-    <h1>
-      <img src={Logo} alt="UOL" className="main_header--logo" />
+    <h1 className="main_header--logo">
+      <Link to="/">
+        <img src={Logo} alt="UOL" />
+      </Link>
     </h1>
   </header>
 );
