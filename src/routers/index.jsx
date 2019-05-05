fffd9e2de32b08bdd8cd2278811996fd5e7100ad
Bruno Macedo <contato@brunomacedo.com.br>
2019-05-05 01:00:46 -0300

    [FEAT] create edit route

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
-import Home from '../containers/Home';
+import {
+  Route, Switch, BrowserRouter, Redirect,
+} from 'react-router-dom';
+import Customers from '../containers/Customers';
+import CustomersForm from '../containers/Customers/Form';
+// import NotFoundPage from '../containers/NotFoundPage';
 
 const MainRouters = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/customers" component={Customers} />
+      <Route exact path="/customers/:id" component={CustomersForm} />
+      <Route render={() => <Redirect to="/customers" />} />
+      {/* <Route component={NotFoundPage} /> */}
     </Switch>
   </BrowserRouter>
 );
