8a0e29eb3f869797d6f168c92a350bed1ca0fa80
Bruno Macedo <contato@brunomacedo.com.br>
2019-05-05 21:27:09 -0300

    [FEAT] create action to add and edit customers

--- src/store/actions/customers.js
+++ src/store/actions/customers.js
@@ -1,6 +1,8 @@
 import { APIResquest } from '../../utils/api';
 
 export const GET_CUSTOMERS = 'GET_CUSTOMERS';
+export const ADD_CUSTOMER = 'ADD_CUSTOMER';
+export const UPDATE_CUSTOMER = 'UPDATE_CUSTOMER';
 export const FETCH_ERROR = 'FETCH_ERROR';
 
 export function loadCustomers() {
@@ -10,8 +12,26 @@ export function loadCustomers() {
       method: 'GET',
     })
       .then(({ data }) => dispatch({
-        type: GET_CUSTOMERS, payload: data,
+        type: GET_CUSTOMERS,
+        payload: data,
       }))
-      .catch(err => dispatch({ type: FETCH_ERROR, payload: err }));
+      .catch(err => dispatch({
+        type: FETCH_ERROR,
+        payload: err,
+      }));
   };
+}
+
+export function addCustomer(data) {
+  return dispatch => dispatch({
+    type: ADD_CUSTOMER,
+    payload: data,
+  });
+}
+
+export function updateCustomer(data) {
+  return dispatch => dispatch({
+    type: UPDATE_CUSTOMER,
+    payload: data,
+  });
 }
